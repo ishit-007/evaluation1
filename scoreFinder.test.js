1090eb1d11f06db6c25f199fc51fa73eb6671fa4
Ishit <ishitsingh2001@gmail.com>
2023-01-18 13:28:40 +0530

    feat : add Best Score capability and add unit tests

--- scoreFinder.test.js
+++ scoreFinder.test.js
@@ -1,4 +1,4 @@
-const {getScore}=require('./scoreFinder');
+const {getScore,getBestScore}=require('./scoreFinder');
 describe("Find Score",()=>{
     it("should throw a TypeErron when provided input is not an array",()=>{
         expect(()=>getScore("abc")).toThrow(TypeError);
@@ -13,5 +13,18 @@ describe("Find Score",()=>{
     })
     it("should return 16  when input is [6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]",()=>{
         expect(getScore([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(16);
+    })
+})
+describe("Find Best Score",()=>{
+    it("should throw a TypeErron when provided input is not an array",()=>{
+        expect(()=>getBestScore("abc")).toThrow(TypeError);
+        expect(()=>getBestScore(123)).toThrow(TypeError);
+        expect(()=>getBestScore({name:"Ishit"})).toThrow(TypeError);
+    })
+    it("should return 9  when input is [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]",()=>{
+        expect(getBestScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(9);
+    })
+    it("should return 30  when input is [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]",()=>{
+        expect(getBestScore([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toBe(30);
     })
 })
